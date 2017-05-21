9f0819417466ea0a6be02df3760b157cc0091d20
harishsrinivas <harishsrinivas89@gmail.com>
2017-05-21 14:37:33 -0700

    Delete Area_Value.R

--- Area_Value.R
+++
@@ -1,8 +0,0 @@
-require(zoo)
-library(stringr)
-
-# Load csv and set empty space with NA
-Dirty.Data <- read.csv("C:/MS Analytics/RStudio Program/dirty_data.csv",  header=T)
-
-# Replace NA in Area column with previous not NULL value
-Area.Val<- transform(Dirty.Data, Area = na.locf(Area))
